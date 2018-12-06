4cb2417602acc319744cb4d89b46f21c9a8edc2e
k.kornienko <kirk.kornienko@gmail.com>
2018-12-06 14:35:11 +0200

    221

--- MerchantApp/DeKee.Base/Entities/Address/Address.cs
+++ MerchantApp/DeKee.Base/Entities/Address/Address.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DeKee.Base.Entities.Address
 {
-    //Test Comment 1
+    //Test Comment 12
     public class Level4 : BaseEntityGeneric<long>
     {
         public string Code { get; set; }
